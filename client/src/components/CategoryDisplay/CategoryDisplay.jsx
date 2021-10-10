4a85cee609e95fea5f5d0a219c7bcfde5c0ac22d
hachi-nico <nicokun48@gmail.com>
2021-10-10 20:42:54 +0700

    add categories, some fix

--- client/src/components/CategoryDisplay/CategoryDisplay.jsx
+++ client/src/components/CategoryDisplay/CategoryDisplay.jsx
@@ -1,29 +1,45 @@
-import React, { useState } from 'react'
-import "./CategoryDisplay.css"
-import { Container } from 'react-bootstrap'
-import animals from '../../assets/images'
+import React, { useState } from 'react';
+import './CategoryDisplay.css';
+import { Container } from 'react-bootstrap';
+import animals from '../../assets/images';
+import { Link } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
 
-import { Link } from "react-router-dom"
-
+const FETCH_CATEGORIES = gql`
+  {
+    categories {
+      id
+      image
+      category
+      slug
+    }
+  }
+`;
 
 function CategoryDisplay() {
+  const { loading, error, data } = useQuery(FETCH_CATEGORIES);
+  if (loading) return <div> </div>;
+  if (error) return <div>error</div>;
 
-    return (
-        <div className="CategoryDisplay">
-            <Container className="CategoryDisplay-container">
-                {[].map(category => {
-                    return (
-                        <Link to={`/products/${category.slug}`} className="CategoryDisplay-card-container">
-                            <div className="CategoryDisplay-card">
-                                <img src={animals[category.img]} /> 
-                            </div>
-                            <h3>{category.category}</h3>
-                        </Link>
-                    )
-                })}
-            </Container>
-        </div>
-    )
+  return (
+    <div className="CategoryDisplay">
+      <Container className="CategoryDisplay-container">
+        {data.categories.map((category) => {
+          return (
+            <Link
+              to={`/products/${category.slug}`}
+              className="CategoryDisplay-card-container"
+            >
+              <div className="CategoryDisplay-card">
+                <img src={animals[category.image]} />
+              </div>
+              <h3>{category.category}</h3>
+            </Link>
+          );
+        })}
+      </Container>
+    </div>
+  );
 }
 
-export default CategoryDisplay
+export default CategoryDisplay;
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -8,6 +8,7 @@ const FETCH_ANIMAL_CARDS = gql`
   {
     animals {
       image
+      title
       id
       price
       slug
