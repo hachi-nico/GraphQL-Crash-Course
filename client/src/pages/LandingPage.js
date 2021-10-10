fb477ce77643e1a57b4d3f376be937c44600a72c
hachi-nico <nicokun48@gmail.com>
2021-10-10 20:35:17 +0700

    done main hero, animal cards

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,17 +1,32 @@
-import React from 'react'
-import MainHero from "../components/MainHero/MainHero"
-import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay"
-import CardDisplay from "../components/CardDisplay/CardDisplay"
+import React from 'react';
+import MainHero from '../components/MainHero/MainHero';
+import CategoryDisplay from '../components/CategoryDisplay/CategoryDisplay';
+import CardDisplay from '../components/CardDisplay/CardDisplay';
+import { useQuery, gql } from '@apollo/client';
+
+const FETCH_ANIMAL_CARDS = gql`
+  {
+    animals {
+      image
+      id
+      price
+      slug
+    }
+  }
+`;
 
 function LandingPage() {
+  const { loading, error, data } = useQuery(FETCH_ANIMAL_CARDS);
+  if (loading) return <div> </div>;
+  if (error) return <div>error</div>;
 
-    return (
-        <div>
-            <MainHero />
-            <CategoryDisplay />
-            <CardDisplay animals={[]}/>
-        </div>
-    )
+  return (
+    <div>
+      <MainHero />
+      <CategoryDisplay />
+      <CardDisplay animals={data.animals} />
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
